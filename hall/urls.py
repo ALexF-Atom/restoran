f36c4141f0aaa3208002f86f0ab3ceae02f882ad
AlexFrundin <aleksey.frundin@gmail.com>
2020-11-05 16:14:26 +0300

    Rewrite FBV to CBV

--- hall/urls.py
+++ hall/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.index, name='index'),
-path('<str:date>', views.tables_view, name='tables-view'),
+path('', views.IndexView.as_view(), name='index'),
+path('<str:date>', views.TablesView.as_view(), name='tables-view'),
 ]
--- hall/views.py
+++ hall/views.py
@@ -1,32 +1,52 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+from django.db.models import Q
+from django.views.generic import View, ListView
 from django.contrib import messages
-from .models import Reservation, Table, UserReserved
+from .models import Reservation, Table
 from .forms import UserReservedForm
 from datetime import datetime
-import json
 
-def index(request):
-    print('index')
-    date = datetime.strftime(datetime.now(),'%Y-%m-%d')
-    if request.method == 'POST':
-        date=request.POST['date']
-    return redirect(tables_view, date)
 
-def tables_view(request, date):
-    user_form = UserReservedForm()
-    all_table = Table.objects.all()
-    if request.method == 'POST':
+class IndexView(View):
+
+    def get(self, request):
+        date = datetime.strftime(datetime.now(), '%Y-%m-%d')
+        return redirect('tables-view', date=date)
+
+    def post(self, request):
+        date = request.POST['date']
+        return redirect('tables-view', date=date)
+
+
+class TablesView(ListView):
+    template_name = 'base.html'
+    context_object_name = 'all_table'
+    model = Table
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['reserved_table'] = tuple(
+            Reservation.objects.filter(
+                data_reserved=self.kwargs['date']).values_list('table_id'))
+        context['form'] = UserReservedForm()
+        context['date'] = self.kwargs['date']
+        return context
+
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
         number_table = request.POST.get('table')
         data_reserved = request.POST.get('data_reserved')
-        if not Reservation.objects.filter(data_reserved=data_reserved).filter(table__number=number_table).exists():
+        if not Reservation.objects.filter(
+            Q(data_reserved=data_reserved) & Q(table__number=number_table)
+                ).exists():
             user_form = UserReservedForm(request.POST)
             if user_form.is_valid():
                 user = user_form.save()
-            r = Reservation(user=user, table=Table.objects.get(number=number_table), data_reserved=data_reserved)
+            r = Reservation(user=user, table=Table.objects.get(
+                number=number_table), data_reserved=data_reserved)
             r.save()
             messages.add_message(request, messages.INFO, "Cтолик забронирован")
         else:
-            messages.add_message(request, messages.ERROR, "Этот столик забронирован")
-    reserved_table = tuple(Reservation.objects.filter(data_reserved=date).values_list('table_id'))
-    context = {'date':date, 'all_table':all_table, 'reserved_table':reserved_table, 'form':user_form}
-    return render(request, 'base.html', context )
+            messages.add_message(request, messages.ERROR,
+                                 "Этот столик забронирован")
+        return self.get(request)
