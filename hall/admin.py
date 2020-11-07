377abc020e6c32591ed332a4c8d46c4a0fc9baa7
AlexFrundin <aleksey.frundin@gmail.com>
2020-11-07 18:46:57 +0300

    Formatted the code according to PEP

--- hall/admin.py
+++ hall/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import  Table, Reservation, UserReserved
+from .models import Table, Reservation, UserReserved
 # Register your models here.
 
 admin.site.register(Table)
--- hall/models.py
+++ hall/models.py
@@ -3,29 +3,35 @@ from django.db import models
 
 class ProcentField(models.IntegerField):
 
-    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+    def __init__(self, verbose_name=None, name=None,
+                 min_value=None, max_value=None, **kwargs):
         self.min_value, self.max_value = min_value, max_value
         models.IntegerField.__init__(self, verbose_name, name, **kwargs)
 
     def formfield(self, **kwargs):
-        defaults = {"min_value": self.min_value, "max_value":self.max_value}
+        defaults = {"min_value": self.min_value, "max_value": self.max_value}
         defaults.update(kwargs)
         return super(ProcentField, self).formfield(**defaults)
 
 
 class Table(models.Model):
 
-    SHAPES=(
+    SHAPES = (
         (0, "ellipse"), (1, "rectangle")
     )
 
     number = models.CharField(verbose_name="Номер", unique=True, max_length=10)
-    y_coordinate = ProcentField(verbose_name="Расположение по вертикали", min_value=0, max_value=100)
-    x_coordinate = ProcentField(verbose_name="Расположение по горизонтали", min_value=0, max_value=100)
-    width = ProcentField(verbose_name="Размер по горизонтали", min_value=1, max_value=100)
-    length = ProcentField(verbose_name="Размер по вертикали", min_value=1, max_value=100)
-    #get_shape_display
-    shape = models.SmallIntegerField(verbose_name="Форма", default=1, choices=SHAPES)
+    y_coordinate = ProcentField(
+        verbose_name="Расположение по вертикали", min_value=0, max_value=100)
+    x_coordinate = ProcentField(
+        verbose_name="Расположение по горизонтали", min_value=0, max_value=100)
+    width = ProcentField(verbose_name="Размер по горизонтали",
+                         min_value=1, max_value=100)
+    length = ProcentField(verbose_name="Размер по вертикали",
+                          min_value=1, max_value=100)
+    # get_shape_display
+    shape = models.SmallIntegerField(
+        verbose_name="Форма", default=1, choices=SHAPES)
     seats = models.IntegerField(verbose_name="Количество мест")
 
     class Meta:
@@ -37,9 +43,7 @@ class Table(models.Model):
         return f"{self.number}"
 
     def get_style_css(self):
-        return f"display: inline-block; width:{self.width}%; height:{self.length}%;"
-
-
+        return (f"display: inline-block; width:{self.width}%; height:{self.length}%;")
 
 
 class UserReserved(models.Model):
@@ -59,8 +63,10 @@ class Reservation(models.Model):
     class Meta:
         unique_together = ['table', 'data_reserved']
 
-    user = models.ForeignKey('UserReserved', on_delete=models.CASCADE, related_name='reserved')
-    table = models.ForeignKey('Table',  on_delete=models.CASCADE, related_name='reserved')
+    user = models.ForeignKey(
+        'UserReserved', on_delete=models.CASCADE, related_name='reserved')
+    table = models.ForeignKey(
+        'Table',  on_delete=models.CASCADE, related_name='reserved')
     data_reserved = models.DateField()
 
     def __str__(self):
